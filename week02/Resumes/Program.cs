51d3f0085ee5e1229bdae0d6becd291bb4ec3389
stranger37 <jvitor_aledre@hotmail.com>
2025-01-21 21:12:56 -0300

    Finished Week 02 assignment for Resume exercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job Job1 = new Job();
+        Job1._company = "Microsoft";
+        Job1._jobtitle = "Software Engineer";
+        Job1._startYear = 2019;
+        Job1._endYear = 2022;
+
+        Job Job2 = new Job();
+        Job2._company = "Apple";
+        Job2._jobtitle = "Manager";
+        Job2._startYear = 2022;
+        Job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "João Viana";
+        myResume._jobs.Add(Job1);
+        myResume._jobs.Add(Job2);
+
+        myResume.Display();
     }
 }
